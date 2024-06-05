1b27b3825bef07ad9f8846be8146fc2415a124e5
Venky7680 <168903586+Venky7680@users.noreply.github.com>
2024-06-05 08:46:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const textBox=document.getElementById("textBox");
+const toCelsius=document.getElementById("toCelsius");
+const toFahrenheit=document.getElementById("toFahrenheit");
+const result=document.getElementById("result");
+let temp;
+
+function convert(){
+    if(toFahrenheit.checked){
+      temp=Number(textBox.value);
+      temp=temp*9/5+32;
+      result.textContent=temp+"°F";
+    }
+    else if(toCelsius.checked){
+        
+        temp=Number(textBox.value);
+        temp=(temp-32)*(5/9);
+        result.textContent=temp+"°C";
+        
+    }
+    else{
+        result.textContent="Select a unit";
+    }
+}
